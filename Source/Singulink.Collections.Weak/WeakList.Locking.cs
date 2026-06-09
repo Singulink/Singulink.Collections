a11feb65b93c978e921ae755c519179a1d85e85e
Hamish Arblaster <hamarb123@gmail.com>
2026-06-09 21:09:14 +1000

    Bug fix for potentially reading stale size/version values

--- Source/Singulink.Collections.Weak/WeakList.Locking.cs
+++ Source/Singulink.Collections.Weak/WeakList.Locking.cs
@@ -41,6 +41,7 @@ public sealed partial class WeakList<T>
             {
                 wasDisposed = _head == null;
                 if (wasDisposed) _locker.Exit();
+                else Thread.MemoryBarrier(); // Most of the methods rely on size or version, so insert a memory barrier to ensure we cannot read stale values for those.
                 return wasDisposed ? default : new LockScope(_locker, this);
             }
 
@@ -63,6 +64,7 @@ public sealed partial class WeakList<T>
             wasDisposed = _head == null;
             entered = !wasDisposed;
             if (wasDisposed) _locker.Exit();
+            else Thread.MemoryBarrier(); // Most of the methods rely on size or version, so insert a memory barrier to ensure we cannot read stale values for those.
             return wasDisposed ? default : new LockScope(_locker, this);
         }
 
